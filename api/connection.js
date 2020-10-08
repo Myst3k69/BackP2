0e5738ef6ab8b3ce16ee254bc757cd7456c0a6d0
Aurélien Chiren <aurelien.chiren@gmail.com>
2020-10-08 22:15:10 +0200

    Update connection.js

--- api/connection.js
+++ api/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 //Connect with DB
 
 const connectDB = () => {
-mongoose.connect('mongodb+srv://p2:p2mdpb@cluster0.vf2je.mongodb.net/shakeit?retryWrites=true&w=majority', { useNewUrlParser: true },{ useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://p2:YOURMDP@cluster0.vf2je.mongodb.net/shakeit?retryWrites=true&w=majority', { useNewUrlParser: true },{ useUnifiedTopology: true })
 
 mongoose.connection.once('open', () => console.log("Connection success")).on('error', (error) => console.log("error is :",error))
 
